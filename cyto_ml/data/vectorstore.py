64c34eea49d61796be8769c143519a4eebdf975c
Jo Walsh <jowals@ceh.ac.uk>
2024-06-25 15:26:00 +0100

    clear out linting issues

--- cyto_ml/data/vectorstore.py
+++ cyto_ml/data/vectorstore.py
@@ -1,18 +1,22 @@
 import chromadb
-from chromadb.db.base import NotFoundError, UniqueConstraintError
+from chromadb.db.base import UniqueConstraintError
 from typing import Optional
+import logging
+logging.basicConfig(level=logging.INFO)
 
 client = chromadb.PersistentClient(path="./vectors")
 
-def vector_store(name: Optional[str] = 'test_collection'):
+
+def vector_store(name: Optional[str] = "test_collection"):
     """
     Return a vector store specified by name, default test_collection
     """
     try:
         collection = client.create_collection(
-            name=name, metadata={"hnsw:space": "cosine"}  # l2 is the default
+            name=name, metadata={"hnsw:space": "cosine"}  # default similarity
         )
     except UniqueConstraintError as err:
         collection = client.get_collection(name)
-    
+        logging.info(err)
+
     return collection
