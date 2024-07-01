dec30c808dfc279a6963e61993c39e39dfacd062
Jo Walsh <jowals@ceh.ac.uk>
2024-07-01 14:10:48 +0100

    adapt script to run through the whole image collection

--- scripts/image_embeddings.py
+++ scripts/image_embeddings.py
@@ -6,10 +6,12 @@ from cyto_ml.models.scivision import (
     load_model,
     truncate_model,
     prepare_image,
+    flat_embeddings,
     SCIVISION_URL,
 )
 from cyto_ml.data.vectorstore import vector_store
 from scivision import load_dataset
+from intake_xarray import ImageSource
 
 load_dotenv()
 
@@ -20,17 +22,28 @@ if __name__ == "__main__":
     # https://github.com/AnnaLinton/scivision_examples/blob/main/how-to-use-scivision.ipynb
 
     dataset = load_dataset(f"{os.environ.get('ENDPOINT', '')}/metadata/intake.yml")
-
-    imgs = dataset.test_image().to_dask()  # this will read a single image as an xarray
-
-    vecs = vector_store()
+    collection = vector_store("plankton")
 
     model = truncate_model(load_model(SCIVISION_URL))
 
-    embeddings = model(prepare_image(imgs))
-
-    print(embeddings)
-
     plankton = (
-        dataset.plankton().to_dask()
+        dataset.plankton().to_dask().compute()
     )  # this will read a CSV with image locations as a dask dataframe
+
+    # Feels like this is doing dask wrong, compute() should happen later
+    # If it doesn't, there are complaints about meta= return value inference
+    # that suggest this is wrongheaded use of `apply`: need to learn better patterns
+    # So this is a kludge, but we're still very much in prototype territory -
+    # Come back and refine this if the next parts work!
+
+    def store_embeddings(row):
+        image_data = ImageSource(row.Filename).to_dask()
+        embeddings = flat_embeddings(model(prepare_image(image_data)))
+        collection.add(
+            documents=[row.Filename],
+            embeddings=[embeddings],
+            ids=[row.Filename],  # must be unique
+            # Note - optional arg name is "metadatas" (we don't have any)
+        )
+
+    plankton.apply(store_embeddings, axis=1)
