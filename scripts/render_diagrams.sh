e6e63dfd200492b56ee50baf66f42abd32111def
Jo Walsh <jowals@ceh.ac.uk>
2024-07-11 11:03:48 +0100

    tweak graph render to output where Jekyll does

--- scripts/render_diagrams.sh
+++ scripts/render_diagrams.sh
@@ -3,6 +3,7 @@
 
 # Set the directory path
 DIR="./docs/diagrams/"
+SITE="_site/"
 
 # Loop through each subdirectory
 for sub_dir in "$DIR"*/; do
@@ -10,12 +11,15 @@ for sub_dir in "$DIR"*/; do
     for dotfile in "$sub_dir"*.dot; do
         # Get the base name without extension
         base_name=$(basename "$dotfile" .dot)
-        
+        dir_path=${sub_dir//docs/docs\/_site}
+        mkdir -p $dir_path
+        output="$dir_path$base_name.svg"
+
         # Render the dot file to SVG
-        dot -Tsvg "$dotfile" -o "$sub_dir$base_name.svg"
+        dot -Tsvg "$dotfile" -o $output
         
         # Print a success message
-        echo "Rendered $dotfile to $sub_dir$base_name.svg"
+        echo "Rendered $dotfile to $output"
     done
 done
 
