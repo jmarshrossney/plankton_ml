bc7c43bc989aaa07c42f9d870b519b3f6888a77d
marshrossney <17361029+marshrossney@users.noreply.github.com>
2024-08-24 13:08:56 +0100

    load scivision model from forked repo

--- tests/test_image_embeddings.py
+++ tests/test_image_embeddings.py
@@ -2,7 +2,6 @@ from intake_xarray import ImageSource
 from torch import Tensor
 from cyto_ml.models.scivision import prepare_image, flat_embeddings
 
-
 def test_embeddings(scivision_model, single_image):
     features = scivision_model(prepare_image(ImageSource(single_image).to_dask()))
 
